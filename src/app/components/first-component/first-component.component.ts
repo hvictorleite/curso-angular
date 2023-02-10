1e6d6d8491b93dfa45f5c6a8cffddd700065fb28
Victor Hugo Marques Rosa Leite <hvictorleite@gmail.com>
2023-02-10 15:35:02 -0300

    Interpolation on first-component html and ts

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./first-component.component.css']
 })
 export class FirstComponentComponent {
-
+  name: string = 'Matheus';
+  age: number = 25;
+  job: string = 'Programador';
+  hobbies: string[] = ['Correr', 'Jogar', 'Estudar'];
+  car = {
+    name: 'Polo',
+    year: '2019'
+  };
 }
